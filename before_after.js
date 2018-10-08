2ede3724723a3cb9900f53e2c76f4b0a72c4e4c7
Kyelandj <kjackson@wfpl.org>
2018-10-08 17:56:07 -0400

    Uploaded photo interactive of DQ's Ceremony

---
+++ before_after.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  // When you click on the before image, fade that image out
+
+  $(".before").click(function(){
+  	$(".before").fadeOut(2000);
+
+  });
+
+
+ $(".after").click(function(){
+    $(".before").fadeIn(2000);
+  });
+
+  // When you click on the after image, fade the before image back in
+
+    
+});
